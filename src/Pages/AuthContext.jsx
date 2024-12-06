f5f01f1c1f9e670d5a7aca18c8f230417e7c0467
pedro-crs <crs.pedroh@gmail.com>
2024-12-06 14:33:46 -0300

    Update: Stay logged in functionality

--- src/Pages/AuthContext.jsx
+++ src/Pages/AuthContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
+import api from "../api/api";
 
 export const AuthContext = createContext();
 
@@ -6,11 +7,21 @@ export const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
 
 	useEffect(() => {
-		const userData = JSON.parse(sessionStorage.getItem("user"));
+		const token = localStorage.getItem("authToken") || sessionStorage.getItem("authToken");
 
-		if (userData)
-			setUser(userData);
+		if (token) {
+			api.get("/auth/validate-token").then((response) => {
+				const { userId, userName } = response.data;
+				setUser({ id: userId, name: userName });
 
+				sessionStorage.setItem("user", JSON.stringify({ id: userId, name: userName }));
+			}).catch(() => {
+				localStorage.removeItem("authToken");
+				sessionStorage.removeItem("authToken");
+				sessionStorage.removeItem("user");
+				setUser(null);
+			});
+		}
 	}, []);
 
 	return (
